cff8abe5ebaa23d81011b32d1415165546c41113
JaoodxD <jaood96@gmail.com>
2022-09-06 23:36:14 +0300

    add import cache comparison example

---
+++ import-cache/index.js
@@ -0,0 +1,5 @@
+import m1 from './simplemodule.js';
+import m2 from './simplemodule.js';
+
+
+console.log(m1===m2);
---
+++ import-cache/simplemodule.js
@@ -0,0 +1,3 @@
+const hello = () => console.log('Zdarova');
+
+export default hello;
