755fd030319eb3af6d4e66d8fd6a3da766382be9
JaoodxD <jaood96@gmail.com>
2022-10-27 13:56:59 +0300

    add lower thrashold to ppf

--- ppf-exponential-distribution.js
+++ ppf-exponential-distribution.js
@@ -1,14 +1,18 @@
-const n = 12;
+const n = 140;
 
 const arr = new Array(n).fill(0).map((_, i) => i);
 
-const f = (n, len) => {
+const f = (n, len, thrashold = 0.2) => {
     const l = len + 1;
     const BETA = 0.40;
     const p = (l - n - 1) / l;
-    return (-BETA * Math.log(1 - p)).toFixed(2);
-}
+    const result = (-BETA * Math.log(1 - p)).toFixed(2);
+    return result > thrashold ? result : thrashold;
+};
 
-const res = arr.map((n, _, arr) => f(n, arr.length));
+const f2 = (n, len, [min, avg, max]) => n < len / 4 ? min : n < (len / 4 * 3) ? avg : max;
 
-console.table([arr, res]);
+const res = arr.map((n, _, arr) => f2(n, arr.length, [1, 2, 3]).toFixed(2));
+
+// console.table([arr, res]);
+console.log(res.map(x => x.replace('.', ',')).join('\n'));
