46605beb65f1c40b4df52159ac7c7c836de1a7b7
JaoodxD <jaood96@gmail.com>
2022-10-27 17:13:41 +0300

    add logarithmic generation of beta coefficient

--- ppf-exponential-distribution.js
+++ ppf-exponential-distribution.js
@@ -1,4 +1,4 @@
-const n = 140;
+const n = 40;
 
 const arr = new Array(n).fill(0).map((_, i) => i);
 
@@ -6,13 +6,14 @@ const f = (n, len, thrashold = 0.2) => {
     const l = len + 1;
     const BETA = 0.40;
     const p = (l - n - 1) / l;
-    const result = (-BETA * Math.log(1 - p)).toFixed(2);
-    return result > thrashold ? result : thrashold;
+    const BB = 0.4868 + 0.873 * Math.log(len);
+    const result = (-Math.log(1 - p) / BB);
+    return result > thrashold ? result.toFixed(2) : thrashold.toFixed(2);
 };
 
 const f2 = (n, len, [min, avg, max]) => n < len / 4 ? min : n < (len / 4 * 3) ? avg : max;
 
-const res = arr.map((n, _, arr) => f2(n, arr.length, [1, 2, 3]).toFixed(2));
+const res = arr.map((n, _, arr) => f(n, arr.length));
 
 // console.table([arr, res]);
-console.log(res.map(x => x.replace('.', ',')).join('\n'));
+console.log(res.map(x => x).join('\n'));
