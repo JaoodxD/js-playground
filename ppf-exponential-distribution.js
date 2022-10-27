c1a49d8ab64fd86a13fae570aa439e8730f5c092
JaoodxD <jaood96@gmail.com>
2022-10-27 20:18:42 +0300

    add minor cleaning from unnecessary code

--- ppf-exponential-distribution.js
+++ ppf-exponential-distribution.js
@@ -1,10 +1,10 @@
-const n = 40;
+const n = 10;
 
-const arr = new Array(n).fill(0).map((_, i) => i);
+const arr = new Array(n).fill(0);
 
 const f = (n, len, thrashold = 0.2) => {
     const l = len + 1;
-    const BETA = 0.40;
+    // const BETA = 0.40;
     const p = (l - n - 1) / l;
     const BB = 0.4868 + 0.873 * Math.log(len);
     const result = (-Math.log(1 - p) / BB);
@@ -13,7 +13,7 @@ const f = (n, len, thrashold = 0.2) => {
 
 const f2 = (n, len, [min, avg, max]) => n < len / 4 ? min : n < (len / 4 * 3) ? avg : max;
 
-const res = arr.map((n, _, arr) => f(n, arr.length));
+const res = arr.map((_, i, arr) => f(i, arr.length));
 
 // console.table([arr, res]);
 console.log(res.map(x => x).join('\n'));
