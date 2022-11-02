10c7e73011e4a924fcb24339b9a012f9ee4e7016
JaoodxD <jaood96@gmail.com>
2022-11-02 12:27:02 +0200

    add comment with example of cli start command

--- perfomance-test.js
+++ perfomance-test.js
@@ -1,10 +1,11 @@
+//node --allow-natives-syntax perfomance-test.js
 "use strict";
 const { performance, createHistogram } = require('perf_hooks');
 var myConstant = 8;
 var result = 0;
 const forLet = () => {
-    var i = 0;
-    for (i = 0; i < 100000000; i++) {
+    // var i = 0;
+    for (let i = 0; i < 100_000_000; i++) {
         result += i + myConstant;
     }
 }
