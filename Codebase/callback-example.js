0b07bc0254587a524753a3e35628ce40910fd2ec
JaoodxD <jaood96@gmail.com>
2022-11-10 17:48:55 +0200

    add extended example of closure

--- Codebase/callback-example.js
+++ Codebase/callback-example.js
@@ -3,17 +3,28 @@
 const Dispatcher = (obj = {}) =>
     (newValue) => {
         obj = { ...obj, ...newValue }
-        console.log({ obj });
+        console.dir({ obj }, { depth: null });
+
     };
 
 const callback = Dispatcher();
 
 // context#1
 {
-    callback({ message: 'Hi' });
+    callback({ message: 'Hi', order: [['id', 'asc']] });
 }
 
 // context#2
 {
-    callback({ someOtherMessage: 'Hello World' });
+    callback({ someOtherMessage: 'Hello World', order: [['name', 'desc']] });
+}
+
+// context#3
+{
+    callback({ someOtherOtherMessage: 'Zdarova Yopta', order: [['country', 'asc']] });
+}
+
+// context#4
+{
+    callback({ id: [1,3,6,8,10], order: [['id', 'desc']] });
 }
