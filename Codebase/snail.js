4e8df3b4eff94bd47cae7fff71da24f5504be24d
JaoodxD <jaood96@gmail.com>
2022-11-29 10:26:21 +0200

    update codewars snail kata

--- Codebase/snail.js
+++ Codebase/snail.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const snail = (matrix) => {
+    if (!matrix[0].length) return [];
     const move = {
         left: [0, -1],
         right: [0, 1],
@@ -43,11 +44,62 @@ const snail = (matrix) => {
     }
     return values;
 };
+const snail2 = (matrix) => {
+    if (!matrix[0].length) return [];
+    const move = {
+        left: [0, -1],
+        right: [0, 1],
+        up: [-1, 0],
+        down: [1, 0]
+    };
+    const turnRight = {
+        right: 'down',
+        down: 'left',
+        left: 'up',
+        up: 'right'
+    };
+    const getValue = ([x, y]) => (matrix[x] || [])[y];
+    const performStep = ([x1, y1], [x2, y2]) => [x1 + x2, y1 + y2];
+
+    const N = matrix[0].length;
+    let currentPoint = [0, 0];
+    let nextPoint = [];
+    let currentDirection = 'right';
+    let currentStep = move[currentDirection];
+
+    const points = [currentPoint];
+
+    while (points.length < N * N) {
+        let [x1, y1] = nextPoint = performStep(currentPoint, currentStep);
+        if (getValue(nextPoint) && !points.some(([x2, y2]) => x1 === x2 && y1 === y2)) {
+            points.push(nextPoint);
+            currentPoint = nextPoint;
+        } else {
+            currentDirection = turnRight[currentDirection];
+            currentStep = move[currentDirection];
+            currentPoint = performStep(currentPoint, currentStep);
+            points.push(currentPoint);
+        }
+    }
+    return points.map(getValue);
+};
+
+function snail3(array) {
+    var vector = [];
+    while (array.length) {
+        vector.push(...array.shift());
+        array.map(row => vector.push(row.pop()));
+        array.reverse().map(row => row.reverse());
+    }
+    return vector;
+}
 
 const array = [
     [1, 2, 3, 4],
     [5, 6, 7, 8],
     [9, 10, 11, 12],
     [13, 14, 15, 16]];
+const array2 = [[]];
+const array3 = [[1]];
 
-console.log(snail(array));
+console.log(snail3(array));
