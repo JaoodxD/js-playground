3f138a7fd7c14c0144f0bc1996d96b4cf3e86343
JaoodxD <jaood96@gmail.com>
2022-12-06 10:07:32 +0200

    add gradient calculation

--- Codebase/interval-normalization/index.js
+++ Codebase/interval-normalization/index.js
@@ -1,13 +1,66 @@
 'use strict';
-const { intervals } = require('./intervals.json');
+let { intervals, gradientConfig } = require('./intervals.json');
+const { watchFile } = require('node:fs');
 
 const seconds = Object.values(intervals).flat();
-console.log({ seconds });
+const getBoundaries = (sec) => {
+    const higherIndex = seconds.findIndex((x) => x >= sec);
+    const lowerIndex = Math.min(0, higherIndex - 1);
+    return [seconds[lowerIndex], seconds[higherIndex]];
+};
 
-const index = seconds.findIndex((n) => n > 1380);
-console.log(index, seconds[index]);
+const min = () => Math.min(...seconds);
+const max = () => Math.max(...seconds);
+const justify = (n) => n > max() ? max() : n < min() ? min() : n;
 
-const distribution = seconds.map((n, i, arr) => ({ dist: i / arr.length, n }));
+const distribution = seconds.reduce((acc, n, i, arr) =>
+    acc.set(n, i / (arr.length - 1)),
+    new Map());
 
-console.table(distribution);
+const f = (x) => distribution.get(x);
 
+const linearInterpolation = (f, x0, x1) => (x) =>
+(
+    f(x0) +
+    (
+        (f(x1) - f(x0))
+        /
+        (x1 - x0)
+    )
+    * (x - x0)
+);
+
+const createGradient = (colors) => (x) => {
+    const color = [...colors].sort(([, x0], [, x1]) => x0 - x1);
+    const getColor = (rgb, x) => {
+        if (!/^[rgb]$/.test(rgb)) throw Error('Invalid color argument');
+        const channel = ['r', 'g', 'b'].indexOf(rgb);
+        const f = (x) => (color.find(([, x0]) => x === x0)?.[0] ?? color[0][0])[channel];
+
+        const higherIndex = color.findIndex(([, x0]) => x0 >= x);
+        const lowerIndex = Math.min(higherIndex - 1, 0);
+
+        return linearInterpolation(f, color[lowerIndex][1], color[higherIndex][1])(x);
+
+    };
+    return [getColor('r', x), getColor('g', x), getColor('b', x)];
+};
+
+let x = 0.00;
+setInterval(() => {
+    if (x === 1) x = 0;
+    x += 0.05;
+    x = Math.min(x, 1);
+    const gradient = createGradient(gradientConfig);
+    const res = gradient(x).reduce((color, channel) => color + (~~channel).toString(16).padStart(2, '0'), "#");
+    console.log('%cTEST', `color: ${res}`, { x, res });
+
+}, 1000);
+
+watchFile('./intervals.json', (eventType, filename) => {
+    // console.log({ eventType, filename });
+    delete require.cache[require.resolve('./intervals.json')];
+    let json = require('./intervals.json');
+    intervals = json.intervals;
+    gradientConfig = json.gradientConfig;
+})
