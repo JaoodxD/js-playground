09b933e9bffb6a42de431f8e1e25a4ae88b292d6
JaoodxD <jaood96@gmail.com>
2023-02-03 12:18:55 +0200

    fix refactoring

--- Codebase/pagination-deck.js
+++ Codebase/pagination-deck.js
@@ -4,7 +4,6 @@ class PaginationDeck {
     #matcher = {};
     constructor(pageSize, totalCount) {
         this.#deck = new Array(pageSize).fill(0);
-        this.#totalCount = totalCount;
         this.#currentPage = 0;
         this.#matcher = Array.from(
             { length: totalCount },
@@ -13,15 +12,22 @@ class PaginationDeck {
             (matcher[page] = (matcher[page] || 0) + 1, matcher), {});
     }
     push(element) {
-        this.#deck.shift();
-        this.#deck.push(element);
+        this.#insertElement(element);
 
-        const elementsCount = this.#deck.filter((x) => x === element).length;
-        const elementsOnPage = this.#matcher[element];
-        if (elementsOnPage === elementsCount) this.#currentPage = element;
+        const isNewPage = this.#checkForNewPage(element);
+        if (isNewPage) this.#currentPage = element;
 
         console.log(this.#currentPage);
         return this.#currentPage;
+    }
+    #insertElement(element) {
+        this.#deck.shift();
+        this.#deck.push(element);
+    }
+    #checkForNewPage(element) {
+        const elementsCount = this.#deck.filter((x) => x === element).length;
+        const elementsOnPage = this.#matcher[element];
+        return (elementsOnPage === elementsCount);
     }
 };
 
