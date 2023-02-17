009180ab82e742cce5b37b708e19bd381c57fcf4
JaoodxD <jaood96@gmail.com>
2023-02-17 17:54:19 +0200

    update state transfer logging

--- Codebase/status-fsm/main.js
+++ Codebase/status-fsm/main.js
@@ -1,37 +1,75 @@
 const STATUSES = {
     'Новый': 'Новый',
     'Принят': 'Принят',
-    'Отказ': 'Отказ',
     'Передан': 'Передан',
     'Возврат': 'Возврат',
     'Завершён': 'Завершён',
 }
+const reserve = () => console.log('+Резерв');
+const unreserve = () => console.log('-Резерв');
+
+const writeOff = () => console.log('-Партии');
+const reverseWriteOff = () => console.log('+Партии');
+
+const assembleOrder = () => console.log('+Собрать заказ');
+const disassembleOrder = () => console.log('-Собрать заказ');
 class FiniteStateMachine {
     state = STATUSES.Новый;
     #transitions = {
         [STATUSES.Новый]: {
             [STATUSES.Принят]() {
                 this.state = STATUSES.Принят;
+                reserve();
             },
             [STATUSES.Передан]() {
                 this.state = STATUSES.Передан;
+                reserve();
+
+                unreserve();
+                assembleOrder();
+                writeOff();
             },
             [STATUSES.Завершён]() {
                 this.state = STATUSES.Завершён;
+                reserve();
+
+                unreserve();
+                assembleOrder();
+                writeOff();
             },
             [STATUSES.Возврат]() {
                 this.state = STATUSES.Возврат;
+                reserve();
+
+                unreserve();
+                assembleOrder();
+                writeOff();
+
+                disassembleOrder();
+                reverseWriteOff();
             },
         },
         [STATUSES.Принят]: {
             [STATUSES.Передан]() {
                 this.state = STATUSES.Передан;
+                unreserve();
+                assembleOrder();
+                writeOff();
             },
             [STATUSES.Завершён]() {
                 this.state = STATUSES.Завершён;
+                unreserve();
+                assembleOrder();
+                writeOff();
             },
             [STATUSES.Возврат]() {
                 this.state = STATUSES.Возврат;
+                unreserve();
+                assembleOrder();
+                writeOff();
+
+                disassembleOrder();
+                reverseWriteOff();
             },
         },
         [STATUSES.Передан]: {
@@ -40,13 +78,15 @@ class FiniteStateMachine {
             },
             [STATUSES.Возврат]() {
                 this.state = STATUSES.Возврат;
+                disassembleOrder();
+                reverseWriteOff();
             },
         },
     };
     changeState(status) {
         if (this.#transitions[this.state] && status in this.#transitions[this.state]) {
+            console.log(`Change state: ${this.state} -> ${status}`);
             this.#transitions[this.state][status].call(this);
-            console.log('New state:', this.state);
         }
         else {
             console.error('ERROR TRANSFER TO:', status);
@@ -54,7 +94,7 @@ class FiniteStateMachine {
     }
 }
 
-console.log('machine#1');
+console.log('\n\nmachine#1');
 const machine = new FiniteStateMachine();
 machine.changeState(STATUSES.Принят);
 machine.changeState(STATUSES.Принят);
@@ -62,9 +102,13 @@ machine.changeState(STATUSES.Завершён);
 machine.changeState(STATUSES.Передан);
 machine.changeState(STATUSES.Завершён);
 
-console.log('machine#2');
+console.log('\n\nmachine#2');
 const machine2 = new FiniteStateMachine();
 machine2.changeState(STATUSES.Принят);
 machine2.changeState(STATUSES.Передан);
 machine2.changeState(STATUSES.Возврат);
 machine2.changeState(STATUSES.Завершён);
+
+console.log('\n\nmachine#3');
+const machine3 = new FiniteStateMachine();
+machine3.changeState(STATUSES.Возврат);
