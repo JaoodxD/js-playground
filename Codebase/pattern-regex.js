aa8f989545b99e7ecab22fd478d5db5fbdd78395
JaoodxD <jaood96@gmail.com>
2023-02-27 16:38:31 +0200

    add pattern matching example

---
+++ Codebase/pattern-regex.js
@@ -0,0 +1,19 @@
+const findWordsInBars = (str) => {
+    const regex = /\|[^\|]+\|/g;
+    const result = [];
+    let match;
+    while ((match = regex.exec(str)) !== null) {
+        result.push([
+            match.index,
+            match.index + match[0].length
+        ]);
+        
+    }
+    return result;
+};
+
+const testStr = '|a1| and |b1| and |a1|';
+
+const res = findWordsInBars(testStr);
+
+console.log({ res });
