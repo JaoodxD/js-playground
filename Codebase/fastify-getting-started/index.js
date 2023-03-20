e80029a7300ed10a8f88a0a03e675ca08569eb7a
JaoodxD <jaood96@gmail.com>
2023-03-20 11:32:53 +0200

    update server to use async/await

--- Codebase/fastify-getting-started/index.js
+++ Codebase/fastify-getting-started/index.js
@@ -4,13 +4,17 @@ const fastify = Fastify({
 });
 
 
-fastify.get('/', (req, res) => {
-  res.send({ hello: 'world' });
-});
+const start = async () => {
+  fastify.get('/', async (req, res) => {
+    return { hello: 'world' };
+  });
 
-fastify.listen({ port: 3000 }, (err, address) => {
-  if (err) {
+  try {
+    await fastify.listen({ port: 3000 });
+  } catch (err) {
     fastify.log.error(err);
     process.exit(1);
   }
-});
+};
+
+start();
