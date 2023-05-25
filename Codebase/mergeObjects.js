8b461281403a78d32e4d322c51f622644d285619
JaoodxD <jaood96@gmail.com>
2023-05-25 11:13:27 +0300

    add object merging implementation

---
+++ Codebase/mergeObjects.js
@@ -0,0 +1,53 @@
+const mergeStrategy = {
+  default: (a, b) => b ?? a,
+  number: (a, b) => b ?? a,
+  string: (a, b) => b ?? a,
+  boolean: (a, b) => b ?? a,
+  object: (obj1, obj2) => {
+    if (!obj1) return merge(obj2, obj2);
+    if (!obj2) return merge(obj1, obj1);
+
+    const { constructor } = Reflect.getPrototypeOf(obj2);
+    const obj = new constructor();
+
+    for (const key in obj1) {
+      obj[key] = merge(obj1[key], obj2[key]);
+    }
+
+    for (const key in obj2) {
+      obj[key] = merge(obj1[key], obj2[key]);
+    }
+    return obj;
+  }
+};
+const getAction = (value) => mergeStrategy[typeof value] ?? mergeStrategy.default;
+const merge = (v1, v2) => getAction(v2)(v1, v2);
+
+const obj1 = {
+  max: 'test',
+  dima: {
+    field1: true
+  },
+  test: [{
+    a: 1,
+    b: '2'
+  },
+  {
+    a: 2,
+    b: '22'
+  }]
+};
+
+const obj2 = {
+  dima: {
+    field1: false,
+    field2: true
+  },
+  test: [{
+    a: 2,
+  }]
+};
+
+const newObj = merge(obj1, obj2);
+
+console.dir({ obj1, obj2, newObj }, { depth: null });
