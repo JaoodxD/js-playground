2ba4b9c9ceba0fe8e2792aeebd0513853b4b309c
JaoodxD <jaood96@gmail.com>
2023-06-12 17:08:26 +0300

    add `config` class prototype

---
+++ Codebase/custom-merging/config.js
@@ -0,0 +1,33 @@
+const merge = require("./customMergeObjects.js");
+
+
+class Config {
+  #config;
+  constructor(cfgObj) {
+    this.#config = cfgObj;
+  }
+
+  diff(cfgObj) {
+    const newCfg = merge(this.#config, cfgObj);
+    return new Diff(newCfg);
+  }
+
+  glue(diff) {
+    return new Config;
+  }
+
+  toPlainObject() {
+    return this.#config;
+  }
+}
+
+class Diff {
+  #configDiff;
+  constructor(cfgDiff) {
+    this.#configDiff = cfgDiff;
+  }
+
+  glue(config) {
+    return config.glue(this.#configDiff);
+  }
+}
