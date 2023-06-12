7826d9f069e50bf4bde3ec39db40d0925efd934a
JaoodxD <jaood96@gmail.com>
2023-06-12 22:17:14 +0300

    add test file to `config` class

---
+++ Codebase/custom-merging/index.js
@@ -0,0 +1,20 @@
+const { Config, Diff } = require('./config');
+const merge = require('./customMergeObjects');
+
+
+const obj1 = {
+  countries: [1, 2, 3, 4, 5],
+
+};
+
+const obj2 = {
+  countries: [3, 4, 6]
+};
+
+const cfg1 = new Config(obj1);
+const cfg2 = new Config(obj2);
+
+const diff = cfg1.diff(cfg2);
+
+console.log(cfg1.toJSON(), cfg2.toJSON(), diff.toJSON());
+console.log(merge(obj1, obj2));
