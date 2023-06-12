0d16cbffbb2248a0600fbb3c85fd1e43489f33b0
JaoodxD <jaood96@gmail.com>
2023-06-12 17:06:36 +0300

    update test-case

--- Codebase/custom-merging/main.js
+++ Codebase/custom-merging/main.js
@@ -2,7 +2,20 @@
 
 const merge = require('./customMergeObjects.js');
 
-const group = require('./group');
+/* const group = require('./group');
 const user = require('./user');
 
 console.dir({ group, user, result: merge(group, user) }, { depth: null });
+ */
+
+
+const arr1 = [1, 2, 3];
+const arr2 = [3, 4];
+
+console.log({ arr1, arr2, result: merge(arr1, arr2) });
+
+const objArr1 = [{ name: 'test', show: true }];
+const objArr2 = [{ name: 'test', show: true }];
+
+console.log({ objArr1, objArr2, result: merge(objArr1, objArr2) });
+
