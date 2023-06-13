52bc32b85d7dc05878c462d4b01f53aabcf6f457
JaoodxD <jaood96@gmail.com>
2023-06-13 22:47:22 +0300

    update test case

--- Codebase/custom-merging/index.js
+++ Codebase/custom-merging/index.js
@@ -2,12 +2,63 @@ const { Config, Diff } = require('./config');
 
 
 const obj1 = {
-  countries: [1, 2, 3, 4, 5],
-
+  countries: [1, 2, 3, 4],
+  order: {
+    settings: {
+      address: {
+        show: true,
+        swap: false
+      }
+    },
+    show: true,
+    time: 1437
+  },
+  cardOrder: {
+    time: 20000,
+    show: true,
+    settings: {
+      comment: {
+        show: false
+      },
+      contact: {
+        rows: [
+          { name: 'country', show: true },
+          { name: 'department', show: true },
+        ],
+        show: true
+      }
+    }
+  }
 };
 
 const obj2 = {
-  countries: [3, 4, 6]
+  countries: [4, 5],
+  order: {
+    settings: {
+      address: {
+        show: false,
+        swap: false
+      }
+    },
+    show: true,
+    time: 1438
+  },
+  cardOrder: {
+    time: 18000,
+    show: true,
+    settings: {
+      comment: {
+        show: true
+      },
+      contact: {
+        rows: [
+          { name: 'asdasd', show: true },
+          { name: 'department', show: true },
+        ],
+        show: true
+      }
+    }
+  }
 };
 
 const cfg1 = new Config(obj1);
@@ -15,4 +66,4 @@ const cfg2 = new Config(obj2);
 
 const diff = cfg1.diff(cfg2);
 
-console.log(cfg1.toJSON(), cfg2.toJSON(), diff.toJSON());
+console.dir(diff.toJSON(), { depth: null });
