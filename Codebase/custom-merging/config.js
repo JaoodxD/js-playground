84bf437c8e7884c5d40371da0172ef77f5447573
JaoodxD <jaood96@gmail.com>
2023-06-16 21:12:00 +0300

    add class to encapsulate diff/merge logic

--- Codebase/custom-merging/config.js
+++ Codebase/custom-merging/config.js
@@ -1,76 +1,26 @@
 const diff = require('./diffObjects');
-
-
-const mergeStrategy = {
-  primitive(value1, value2) {
-    return value2 ?? value1;
-  },
-
-  primitiveArray(arr1, arr2 = []) {
-    const concated = arr1.filter((x) => !arr2.includes(x)).concat(arr2);
-    return concated;
-  },
-
-  objectsArray(arr1, arr2) {
-    const unique1 = arr1.filter((x) => !arr2.some(({ name }) => name === x.name));
-    const unique2 = arr2.filter((x) => !arr1.some(({ name }) => name === x.name));
-
-    const common = arr1.filter((x) => arr2.some(({ name }) => name === x.name));
-    const merged = common.map((obj) => {
-      const commonObj = arr2.find((({ name }) => name === obj.name));
-      const merged = merge(obj, commonObj);
-      return merged;
-    });
-
-    return unique1.concat(merged, unique2);
-  },
-
-  timestampedObject(obj1, obj2 = {}) {
-    const { time: t1 } = obj1;
-    const { time: t2 = 0 } = obj2;
-    if (t1 > t2) return this.object(obj1, {});
-    return this.object(obj1, obj2);
-  },
-
-  object(obj1, obj2) {
-    if (!obj1) return merge(obj2, obj2);
-    if (!obj2) return merge(obj1, obj1);
-    const { constructor } = Reflect.getPrototypeOf(obj2);
-    const obj = new constructor();
-
-    const fields1 = Object.keys(obj1);
-    const fields2 = Object.keys(obj2);
-    const fields = new Set(fields1.concat(fields2));
-
-    for (const key of fields) {
-      const mergedValue = merge(obj1[key], obj2[key]);
-      obj[key] = mergedValue;
-    }
-    return obj;
-  }
-};
-
-const getType = (value) => {
-  if (Array.isArray(value)) {
-    const [element] = value;
-    const type = typeof element;
-    if (type !== 'object') return 'primitiveArray';
-    return 'objectsArray';
-  }
-  if (typeof value === 'object') {
-    if ('time' in value) return 'timestampedObject';
-    return 'object';
-  };
-  return 'primitive';
-}
-
-const merge = (v1, v2) => mergeStrategy[getType(v1)](v1, v2);
-
+const merge = require('./mergeObjects');
+const getType = require('./valueType');
 
 class Config {
   #config;
+
+  #firstParent;
+  #secondParent;
+
+  #CELL_TYPE = {
+    INHERIT: 'grey',
+    ENABLED: 'blue',
+    DISABLED: 'red',
+    NONE: 'transparent'
+  }
+  #cellTypes;
+  #firstParentCells;
+  #secondParentCells;
+
   constructor(cfgObj) {
     this.#config = cfgObj;
+    this.#cellTypes = this.calculateCellTypes(this.#config);
   }
 
   glue(cfgObj) {
@@ -80,8 +30,64 @@ class Config {
   }
 
   diff(config) {
-    const obj = config instanceof Config ? config.toJSON() : config
-    return diff(this.#config, obj);
+    const obj = config instanceof Config ? config.toJSON() : config;
+    const newCfg = diff(this.#config, obj);
+    return new Config(newCfg).setParents(this, config);
+  }
+
+  setParents(first, second) {
+    this.#firstParent = first;
+    this.#secondParent = second;
+    this.#cellTypes = this.calculateCellTypes(this.#config);
+    this.#firstParentCells = this.calculateCellTypes(first.toJSON());
+    this.#secondParentCells = this.calculateCellTypes(second.toJSON());
+    return this;
+  }
+
+  cellType(path) {
+    if (!this.#firstParent || !this.#secondParent) throw new Error('No parents');
+    const firstParentCell = this.#firstParentCells.get(path);
+    const secondParentCell = this.#secondParentCells.get(path);
+    if (!firstParentCell && secondParentCell) return this.#CELL_TYPE.ENABLED;
+    if (firstParentCell && secondParentCell) return this.#CELL_TYPE.INHERIT;
+    if (firstParentCell && !secondParentCell) return this.#CELL_TYPE.DISABLED;
+    return this.#CELL_TYPE.NONE;
+  }
+
+  printCells() {
+    if (this.#firstParent && this.#secondParent) {
+      const keys = new Set(
+        [...this.#firstParentCells.keys()]
+          .concat([...this.#secondParentCells.keys()]));
+      for (const key of keys) {
+        console.log(key, this.cellType(key));
+      }
+    }
+    else {
+      for (const [key] of this.#cellTypes) {
+        console.log(key, this.cellType(key));
+      }
+    }
+  }
+
+  calculateCellTypes(object) {
+    const cellTypes = new Map();
+    for (const key in object) {
+      if (getType(object[key]) === 'primitive') {
+        cellTypes.set(key, 'primitive');
+      }
+      else {
+        const map = this.calculateCellTypes(object[key]);
+        for (const [nestedKey, value] of map) {
+          if (nestedKey === 'time') continue;
+          if (Array.isArray(object[key])) {
+            cellTypes.set(`${key}/${object[key][nestedKey]}`, value);
+          }
+          else cellTypes.set(`${key}/${nestedKey}`, value);
+        }
+      }
+    }
+    return cellTypes;
   }
 
   toJSON() {
