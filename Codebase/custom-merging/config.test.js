8d32eedc3f377e1e019bf00743cba47dbbf5a5dc
JaoodxD <jaood96@gmail.com>
2023-06-20 15:25:41 +0300

    add test cases for diff of array of primitives

--- Codebase/custom-merging/config.test.js
+++ Codebase/custom-merging/config.test.js
@@ -43,3 +43,73 @@ test('diff primitives', async (t) => {
     assert.strictEqual(result, value2);
   });
 });
+
+test('diff array of primitives', async (t) => {
+  await t.test('should return first array if second is empty', () => {
+    const value1 = [1, 2, 3, 4];
+    const value2 = [];
+
+    const cfg1 = new Config(value1);
+    const cfg2 = new Config(value2);
+    const diff = cfg1.diff(cfg2);
+
+    const result = diff.toJSON();
+
+    assert.deepEqual(result, value1);
+  });
+
+  await t.test('should return second if first array is empty', () => {
+    const value1 = [];
+    const value2 = [1, 2, 3, 4];
+
+    const cfg1 = new Config(value1);
+    const cfg2 = new Config(value2);
+    const diff = cfg1.diff(cfg2);
+
+    const result = diff.toJSON();
+
+    assert.deepEqual(result, value2);
+  });
+
+  await t.test('should return excluded elements', () => {
+    const value1 = [1, 2, 3, 4];
+    const value2 = [1, 3];
+
+    const cfg1 = new Config(value1);
+    const cfg2 = new Config(value2);
+    const diff = cfg1.diff(cfg2);
+
+    const result = diff.toJSON();
+    const expected = [2, 4];
+
+    assert.deepEqual(result, expected);
+  });
+
+  await t.test('should return included elements', () => {
+    const value1 = [1, 2, 3, 4];
+    const value2 = [1, 2, 3, 4, 5];
+
+    const cfg1 = new Config(value1);
+    const cfg2 = new Config(value2);
+    const diff = cfg1.diff(cfg2);
+
+    const result = diff.toJSON();
+    const expected = [5];
+
+    assert.deepEqual(result, expected);
+  });
+
+  await t.test('should return newely included and excluded elements', () => {
+    const value1 = [1, 2, 3, 4];
+    const value2 = [1, 2, 3, 5];
+
+    const cfg1 = new Config(value1);
+    const cfg2 = new Config(value2);
+    const diff = cfg1.diff(cfg2);
+
+    const result = diff.toJSON();
+    const expected = [4, 5];
+
+    assert.deepEqual(result, expected);
+  });
+});
