45991c32f2d293fc297ad6b78fb4266945a44de1
JaoodxD <jaood96@gmail.com>
2023-06-22 16:39:25 +0300

    add helper function to simplify `cellType` logic

--- Codebase/custom-merging/config.js
+++ Codebase/custom-merging/config.js
@@ -95,6 +95,29 @@ class Config {
   }
 }
 
+function DimkinHelperSKrasnymiISinimiTochkami(obj1, obj2, path) {
+  const CELL_TYPE = {
+    INHERIT: 'grey',
+    ENABLED: 'blue',
+    DISABLED: 'red',
+    NONE: 'transparent'
+  };
+  const getValue = (obj, path) => path.split('/').reduce((acc, key) => {
+    if (acc == null) return acc;
+    if (Array.isArray(acc)) return acc.find((value) => value == key)
+    return acc[key];
+  }, obj);
+
+  const value1 = getValue(obj1, path);
+  const value2 = getValue(obj2, path);
+
+  if (!value1 && value2) return CELL_TYPE.ENABLED;
+  if (value1 && value2) return CELL_TYPE.INHERIT;
+  if (value1 && !value2) return CELL_TYPE.DISABLED;
+  return CELL_TYPE.NONE;
+};
+
 module.exports = {
-  Config
+  Config,
+  DimkinHelperSKrasnymiISinimiTochkami
 }
