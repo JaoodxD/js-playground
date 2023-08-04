7365d38b51293d75f55f4f2ea56a4a3447d89810
JaoodxD <jaood96@gmail.com>
2023-08-04 18:34:00 +0300

    Add example of comparison regular branching with strategy

---
+++ Codebase/stragety-pattern/1-simple.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const names = ['dmytro', 'illya', 'maksym', 'evgen'];
+
+console.log('IF..IF statements...');
+for (const name of names) {
+  if (name === 'dmytro') {
+    console.log('Dimasik', 10);
+  }
+  if (name === 'illya') {
+    console.log('Ilyuha', 20);
+  }
+  if (name === 'maksym') {
+    console.log('Maksim', 30);
+  }
+  if (name === 'evgen') {
+    console.log('Jenya', 40);
+  }
+}
+
+console.log('\n\nSTRATEGIES...');
+const strategies = {
+  dmytro: (props) => console.log('Dimasik', 10, props),
+  illya: () => console.log('Ilyuha', 20),
+  maksym: () => console.log('Maksim', 30),
+  evgen: () => console.log('Jenya', 40)
+}
+const call = (name, ...props) => strategies[name](...props);
+
+names.map((name) => call(name, { kakoitoProps: 'ASDASASD' }));
