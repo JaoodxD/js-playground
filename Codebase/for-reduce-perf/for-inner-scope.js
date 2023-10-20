460c4f76bd7bffea53f1ca96b3faf6c69e3fa264
JaoodxD <jaood96@gmail.com>
2023-10-20 16:49:11 +0300

    Update perf

--- Codebase/for-reduce-perf/for-inner-scope.js
+++ Codebase/for-reduce-perf/for-inner-scope.js
@@ -1,20 +1,20 @@
-var arr = new Array(100).fill(0).map((e, i) => i);
+var arr = new Array(100).fill(0).map((e, i) => i)
 var res = 0
 
-function myFor(arr) {
+function myFor (arr) {
   var tempSum = 0
   for (let j = 0; j < 10_000_000; j++) {
     for (let index = 0; index < arr.length; index++) {
-      tempSum += arr[index];
+      tempSum += arr[index]
     }
   }
-    return tempSum
+  res += tempSum
 }
 
 for (let c = 0; c < 5; c++) {
   console.time('for')
-  res += myFor(arr)
+  myFor(arr)
   console.timeEnd('for')
 }
 
-console.log(res);
+console.log(res)
