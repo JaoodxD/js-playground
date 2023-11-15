1de29ce0dd1956627e03d6a240a3434af2b22f98
JaoodxD <jaood96@gmail.com>
2023-11-15 11:14:33 +0200

    One more test

---
+++ Codebase/for-reduce-perf/for-outer-scope2.js
@@ -0,0 +1,22 @@
+var arr = new Array(100).fill(0).map((e, i) => i)
+var res = 0
+
+function myFor (arr) {
+  for (let j = 0; j < 10_000_000; j++) {
+    newFunction(arr)
+  }
+}
+
+for (let c = 0; c < 5; c++) {
+  console.time('for')
+  myFor(arr)
+  console.timeEnd('for')
+}
+
+console.log(res)
+function newFunction(arr) {
+  for (let index = 0; index < arr.length; index++) {
+    res += arr[index]
+  }
+}
+
