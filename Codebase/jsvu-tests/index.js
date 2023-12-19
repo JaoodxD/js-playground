375a8732e07955ba4700d516d848e3d816cc34ea
JaoodxD <jaood96@gmail.com>
2023-12-19 12:52:34 +0200

    Add summator example

---
+++ Codebase/jsvu-tests/index.js
@@ -0,0 +1,18 @@
+"use strict";
+function doSum( theInc ) {
+  var theSummator = 0;
+  for ( var theLimit = 0; theLimit < 1073741824; theLimit++ ) {
+    theSummator += theInc;
+  }
+  return theSummator;
+}
+
+var start = performance.now();
+
+console.log(
+  doSum( 1 )
+  , doSum( -1 )
+  , doSum( 0.1 )
+);
+
+console.log(performance.now() - start);
