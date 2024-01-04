697a8f5d591c3327aaa71e9b8e3bc25325d7e1da
JaoodxD <jaood96@gmail.com>
2024-01-04 15:00:18 +0200

    Add test of Murych one sign solution

---
+++ Codebase/sum-perf.js
@@ -0,0 +1,16 @@
+"use strict";
+function doSum( theInc ) {
+  var theSummator = 0;
+  for ( var theLimit = 0; theLimit < 1073741824; theLimit++ ); {
+    theSummator += theInc;
+  }
+  return theSummator;
+}
+console.time('perf');
+console.log(
+  doSum( 1 )
+  , doSum( -1 )
+  , doSum( 0.1 )
+);
+console.timeEnd('perf');
+
