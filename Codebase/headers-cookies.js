fadba1a9ed897e6c0b6bc000564c4659e8d4d35a
JaoodxD <jaood96@gmail.com>
2024-02-06 20:17:23 +0200

    Minor clean-up

--- Codebase/headers-cookies.js
+++ Codebase/headers-cookies.js
@@ -2,8 +2,6 @@ const headers = new Headers()
 headers.append('Set-Cookie', 'key=value')
 headers.append('Set-Cookie', 'key2=value2')
 
-
-
 for (const [key, value] of headers) {
   console.log(key, value)
 }
