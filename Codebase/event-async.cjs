b40a9acf4f3654912ed8a98d8b36122259ee5914
JaoodxD <jaood96@gmail.com>
2024-04-25 22:08:05 +0300

    Add example of catching promise rejections of async eventEmitter callbacks

---
+++ Codebase/event-async.cjs
@@ -0,0 +1,23 @@
+const EventEmitter = require('node:events')
+const { setTimeout: sleep } = require('node:timers/promises')
+
+const ee1 = new EventEmitter({ captureRejections: true })
+ee1.on('something', async value => {
+  await sleep(500)
+  throw new Error('kaboom')
+})
+
+ee1.on('error', console.log)
+
+const ee2 = new EventEmitter({ captureRejections: true })
+ee2.on('something', async value => {
+  await sleep(500)
+  throw new Error('kaboom')
+})
+
+ee2[Symbol.for('nodejs.rejection')] = console.log
+
+;(function doSomething () {
+  ee1.emit('something')
+  // ee2.emit('something else')
+})()
