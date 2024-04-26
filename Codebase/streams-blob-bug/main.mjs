170a60f1fd9016ea139b6eb1b73853c9608a14ac
JaoodxD <jaood96@gmail.com>
2024-04-26 17:34:34 +0300

    Example of bug with transform stream

---
+++ Codebase/streams-blob-bug/main.mjs
@@ -0,0 +1,52 @@
+import { pipeline } from 'node:stream/promises'
+import fs from 'node:fs'
+import stream from 'node:stream'
+import { join } from 'node:path'
+import { randomUUID } from 'node:crypto'
+import { setTimeout as sleep } from 'node:timers/promises'
+
+const folderPath = './storage'
+
+const data = generateData(100)
+
+const paths = await saveFileChunks(data, randomUUID())
+console.log(paths)
+await sleep(2000)
+
+const files = await Promise.all(paths.map(path => fs.openAsBlob(path)))
+
+console.log(files)
+
+async function* generateData (lines = 500) {
+  for (let i = 0; i < lines; i++) {
+    yield JSON.stringify({ hello: 'world', i }) + '\n'
+  }
+}
+
+async function saveFileChunks (inputStream, fileName, maxRows = 500) {
+  const fullPath = join(folderPath, fileName)
+
+  const filesPath = [`${fullPath}-0.json`]
+  let outputStream = fs.createWriteStream(filesPath[0])
+
+  let count = 0
+  const options = {
+    async transform (chunk, encoding, next) {
+      count++
+      outputStream.write(chunk)
+      if (count >= maxRows) {
+        count = 0
+        outputStream.end()
+        const filePath = `${fullPath}-${filesPath.length}.json`
+        filesPath.push(filePath)
+        outputStream = fs.createWriteStream(filePath)
+      }
+      next()
+    }
+  }
+
+  await pipeline(inputStream, new stream.Transform(options))
+  outputStream.end()
+
+  return filesPath
+}
