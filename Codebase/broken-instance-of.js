4725788b4b3d841c1646d6e127d46d4fb3cb20ca
JaoodxD <jaood96@gmail.com>
2024-07-23 12:58:51 +0300

    Add fix by passing original Error object into new context

--- Codebase/broken-instance-of.js
+++ Codebase/broken-instance-of.js
@@ -3,12 +3,16 @@ const vm = require('node:vm')
 const plainObject = {}
 const newContextObject = vm.runInNewContext('({})')
 
-console.log(plainObject, plainObject instanceof Object) // true
-console.log(newContextObject, newContextObject instanceof Object) // false
-
+console.log(plainObject instanceof Object) // true
+console.log(newContextObject instanceof Object) // false
 
 const plainError = new Error()
 const newContextError = vm.runInNewContext('new Error()')
 
-console.log(plainError, plainError instanceof Error) // true
-console.log(newContextError, newContextError instanceof Error) // false
+console.log(plainError instanceof Error) // true
+console.log(newContextError instanceof Error) // false
+
+const customContext = vm.createContext({ Error })
+const customContextError = vm.runInContext('new Error()', customContext)
+
+console.log(customContextError instanceof Error) // true
